0847c1b70493e5fc2fda558aa49758e8a36c8279
Lucas Huang <lucas860802@gmail.com>
2021-06-30 01:59:05 +0800

    calc avg length reviews

--- read.py
+++ read.py
@@ -8,8 +8,10 @@ with open('reviews.txt', 'r')as f:
 		count += 1000
 		if count % 1000 == 0: # %除完餘數等於0
 			print(len(data))
+print('檔案讀取完了，總共有', len(data),'筆資料')
 
-print(len(data))
-print(data[0])
-print('------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('留言平均是', sum_len/len(data))
